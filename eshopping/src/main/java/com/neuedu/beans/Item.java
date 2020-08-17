b7197b3f0e66404642d182ebacf67449fb1752ea
liuwei <liuwei.liu@neusoft.com>
2020-08-17 17:02:09 +0800

    添加商品实体类

---
+++ eshopping/src/main/java/com/neuedu/beans/Item.java
@@ -0,0 +1,26 @@
+package com.neuedu.beans;
+
+public class Item {
+
+	private Integer itemid;
+	
+	private String itemname;
+
+	public Integer getItemid() {
+		return itemid;
+	}
+
+	public void setItemid(Integer itemid) {
+		this.itemid = itemid;
+	}
+
+	public String getItemname() {
+		return itemname;
+	}
+
+	public void setItemname(String itemname) {
+		this.itemname = itemname;
+	}
+	
+	
+}
